3d54a5d5fc2d4f9b24508fc186b5a7929afc9fa0
HuaMinhKhuong-22700971 <khuongminhhua11062004@gmail.com>
2025-10-24 13:54:46 +0700

    Update lai

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -104,15 +104,15 @@ class ProductController {
   }
 
   // Lấy sản phẩm theo ID
-async getProductById(req, res) {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(404).json({ message: "Product not found" });
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-}
+//async getProductById(req, res) {
+  //try {
+    //const product = await Product.findById(req.params.id);
+    //if (!product) return res.status(404).json({ message: "Product not found" });
+    //res.json(product);
+  //} catch (err) {
+    //res.status(500).json({ message: "Server error" });
+  //}
+//}
 
 }
 
